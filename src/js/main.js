35eb7d4e6d5eea1672650dd380af581e451c72ca
tamrat <tamratg23@gmail.com>
2017-04-11 01:32:28 -0400

    added multi frame effect

--- src/js/main.js
+++ src/js/main.js
@@ -3,35 +3,40 @@ var videoContainer
 var effectDisplay
 var btn
 var snaps = []
+var multiSnaps = []
+var multiFrameCounter = 0
 var currentEffect = ''
 
 function setup() {
-  var canvas = createCanvas(620,480)
+  var canvas = createCanvas(640,480)
   videoFeed = createCapture(VIDEO)
   videoFeed.hide()
+  // videoFeed.showControls()
 
   //create effect buttons
-  var btnContainer = createP('').addClass('btn-container')
-  var e1 = createP('Effect 1').addClass('btn').id('effect1').parent(btnContainer)
-  var e2 = createP('snap!').addClass('btn').id('effect2').parent(btnContainer)
+  var btnContainer = createDiv('').addClass('btn-container')
 
-  //attch effects to buttons
-  e1.mousePressed(streamBlue)
-  e2.mousePressed(takeSnap)
+  var emojify = createP('Emogify').addClass('btn').id('emojify').parent(btnContainer)
+  var snap = createP('Snap!').addClass('btn').id('takeSnap').parent(btnContainer)
+  var multiFrame = createP('Multi-Frame').addClass('btn').id('multiFrame').parent(btnContainer)
 
+  //attach effects to buttons
+  emojify.mousePressed(changeEffect)
+  snap.mousePressed(takeSnap)
+  multiFrame.mousePressed(changeEffect)
+
+
+  // var emoji = createP('😢').position(width / 2, height / 2).style('font-size', 24)
 }
 
-function logg(){
-  console.log('clickk');
-}
 
-function streamBlue(){
-  currentEffect = 'streamBlue'
+function changeEffect(event){
+  currentEffect = event.target.id
+  console.log(currentEffect);
 }
 
 function takeSnap(){
   currentEffect = 'takeSnap'
-  tint(color('#e36900'))
   snaps.push(videoFeed.get())
   image(snaps[snaps.length - 1], 0, 0)
 }
@@ -39,10 +44,34 @@ function takeSnap(){
 //p5 draw function called everyframe
 function draw() {
 
-if(currentEffect === 'streamBlue') {
-  tint('#9e2165')
+
+if(currentEffect === 'emojify') {
+  clear()
   image(videoFeed, 0 ,0)
 }
 
 
+if(currentEffect === 'multiFrame' && videoFeed) {
+  multiSnaps[multiFrameCounter] = videoFeed.get()
+  multiFrameCounter ++
+  //41 total no of multi frames possible
+  if(multiFrameCounter == 41) { multiFrameCounter = 0}
+  var w = width / 6
+      h = height / 6
+      x = 0
+      y = 0
+  for (var i = 0; i < multiSnaps.length; i++) {
+    var index = (i + frameCount) % multiSnaps.length
+    image(multiSnaps[index], x, y, w, h)
+    //move to the next frame
+    x += w
+    if (x > width) {
+      x = 0
+      y = y + h
+    }
+  }
 }
+}
+
+
+// tint(random(255),random(255), random(255))
