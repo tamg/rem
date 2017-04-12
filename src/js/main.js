31eae7c0cac22a84ded2a5784f3c993296a13553
tamrat <tamratg23@gmail.com>
2017-04-12 02:12:45 -0400

    Added Tacking and Emoji effect!

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,72 @@
 var videoFeed
 var videoContainer
+var videoScale = 16
 var effectDisplay
-var btn
+var w
+var h
 var snaps = []
 var tintColor = '#00ddff';
 var multiSnaps = []
 var multiFrameCounter = 0
-var currentEffect = ['none', 'emojify', 'takeSnap', 'multiFrame']
+var currentEffect = ['none', 'mirror', 'emojify', 'emojiHead', 'multiFrame']
 // var effects =
-var gui
+var mainGui
+var outputGui
 
-var settings
+var colors
 
 function setup() {
+  w = windowWidth
+  h = windowHeight
+  pixelDensity(1) //disregard retina display
+
   var canvas = createCanvas(640,480)
   videoFeed = createCapture(VIDEO)
-  videoFeed.hide()
-  // videoFeed.showControls()
-  settings = QuickSettings.create(width/2, height/2, "Settings").setWidth(680)
-  settings.bindColor("backgroundColor", "#ffffff", document.body.style);
-  settings.bindDropDown("designMode", ["on", "off"], document)
-  settings.addElement("Element (canvas)", canvas.elt)
+  videoFeed.size(640/videoScale, 480/videoScale).id('myvideo')
+  // videoFeed.hide()
 
-  gui = createGui('Effect GUI')
-  gui.addGlobals('currentEffect', 'tintColor')
-
-  // noLoop()
-  //create effect buttons
-  var btnContainer = createDiv('').addClass('btn-container')
-
-  // var emojify = createP('Emogify').addClass('btn').id('emojify').parent(btnContainer)
-  var snap = createP('Snap!').addClass('btn').id('takeSnap').parent(btnContainer)
-  // var multiFrame = createP('Multi-Frame').addClass('btn').id('multiFrame').parent(btnContainer)
-  //
-  //attach effects to buttons
-  // emojify.mousePressed(changeEffect)
-  snap.mousePressed(takeSnap)
-  // multiFrame.mousePressed(changeEffect)
+  //center it
+  mainGui = QuickSettings.create(w/2 - 330, h/2 - 330, "REM ✌🏽")
+             .setWidth(660)
+             .addDropDown("💥 Choose an Effect 💥", currentEffect, function(val){changeEffect(val.value)})
+             .addElement("", canvas.elt)
+             .addButton("📸 Save Photo", saveImage)
+             .addButton("📹 Record a GIF", saveImage)
+             .addButton("🚀 Upload GIF to IMGUR", saveImage)
 
 
-  // var emoji = createP('😢').position(width / 2, height / 2).style('font-size', 24)
+ colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow'])
+ //tracking
+ colors.on('track', function(event) {
+   if (event.data.length === 0) {
+     // No colors were detected in this frame.
+   } else {
+     event.data.forEach(function(frame) {
+       drawRect(frame)
+     })
+   }
+ })
+
+tracking.track('#myvideo', colors);
+
+}//SETUP
+
+
+
+function drawRect(frame) {
+  noFill()
+  stroke(frame.color)
+  rect(frame.x, frame.y, frame.height, frame.width)
 }
 
 
-function changeEffect(event){
-  currentEffect = event.target.id
-  console.log(currentEffect);
+
+function saveImage() {
+  saveCanvas(canvas, 'myCanvas', 'png');
+}
+
+function changeEffect(value){
+  currentEffect = value
 }
 
 function takeSnap(){
@@ -54,44 +75,72 @@ function takeSnap(){
   image(snaps[snaps.length - 1], 0, 0)
 }
 
-draw
+
 
 //p5 draw function called everyframe
 function draw() {
 
-tint(tintColor)
+  if(currentEffect === 'emojify' && videoFeed) {
+    videoFeed.loadPixels()
+    loadPixels()
+    for(var y = 0; y < videoFeed.height; y++) {
+      for(var x = 0; x < videoFeed.width; x++){
+        var index = (videoFeed.width - x + 1 + (y * videoFeed.width))*4 //reverse image for mirror effect
+        //1 pixel holds 4 values (rgba)
+        var r = videoFeed.pixels[index + 0] //r
+        var g = videoFeed.pixels[index + 1] //g
+        var b = videoFeed.pixels[index + 2] //b
 
+        var brightness = (r + g + b)/3
 
+        var emoji = ''
 
-if(currentEffect === 'emojify') {
-  noTint()
-  clear()
-  image(videoFeed, 0 ,0)
-}
+        if (brightness < 100) {
+          emoji = '🎱'
+        } else if (brightness > 100 && brightness < 200) {
+          emoji = '😢'
+        } else if (brightness > 100 && brightness < 200) {
+          emoji = '🔥'
+        }
 
-
-if(currentEffect === 'multiFrame' && videoFeed) {
-  noTint()
-  multiSnaps[multiFrameCounter] = videoFeed.get()
-  multiFrameCounter ++
-  //41 total no of multi frames possible
-  if(multiFrameCounter == 41) { multiFrameCounter = 0}
-  var w = width / 6
-      h = height / 6
-      x = 0
-      y = 0
-  for (var i = 0; i < multiSnaps.length; i++) {
-    var index = (i + frameCount) % multiSnaps.length
-    image(multiSnaps[index], x, y, w, h)
-    //move to the next frame
-    x += w
-    if (x > width) {
-      x = 0
-      y = y + h
+        text(emoji, x*videoScale,y*videoScale,videoScale,videoScale)
+      }
     }
   }
-}
-}
+
+
+  if(currentEffect === 'multiFrame' && videoFeed) {
+    noTint()
+    videoFeed.size(640, 480)
+    multiSnaps[multiFrameCounter] = videoFeed.get()
+    multiFrameCounter ++
+    //41 total no of multi frames possible
+    if(multiFrameCounter == 41) { multiFrameCounter = 0}
+    var w = width / 6
+        h = height / 6
+        x = 0
+        y = 0
+    for (var i = 0; i < multiSnaps.length; i++) {
+      var index = (i + frameCount) % multiSnaps.length
+      image(multiSnaps[index], x, y, w, h)
+      //move to the next frame
+      x += w
+      if (x > width) {
+        x = 0
+        y = y + h
+      }
+    }
+  }
+}// DRAW
 
 
 // tint(random(255),random(255), random(255))
+// mainGui.bindDropDown("designMode", ["on", "off"], document)
+// mainGui.bindColor("backgroundColor", "#ffffff", document.body.style);
+
+
+//black and white mirror
+// pixels[index + 0] = brightness
+// pixels[index + 1] = brightness
+// pixels[index + 2] = brightness
+// pixels[index + 3] = 255//a
